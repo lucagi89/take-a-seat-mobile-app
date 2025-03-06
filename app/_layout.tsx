bd6a0e91fa05b2a348ca68f26c6f2e3408d64cb2
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2025-03-06 00:07:53 +0000

    start to set stripe payments and create an express server for that

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,19 @@
 import { Stack } from "expo-router";
 import { useUser, UserContextProvider } from "../contexts/userContext";
-import { Slot, useRouter } from "expo-router";
+import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function RootLayout() {
-
   return (
     <UserContextProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Slot />
-        </Stack>
-      </SafeAreaView>
+      <StripeProvider publishableKey="pk_test_YourStripePublicKey">
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Slot />
+          </Stack>
+        </SafeAreaView>
+      </StripeProvider>
     </UserContextProvider>
   );
 }
