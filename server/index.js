dacd332eca0082f0e48d7f5a4bdde39b1526c107
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2025-03-07 22:23:33 +0000

    start to set stripe payments and create an express server for that

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 require("dotenv").config();
+import express, { json } from "express";
+import cors from "cors";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors());
-app.use(express.json()); // ✅ Make sure this is included
+app.use(json()); // ✅ Make sure this is included
 
 app.post("/create-payment-intent", async (req, res) => {
     try {
         const { amount, currency, userId } = req.body;
 
-        if (!amount || !currency) {
+        if (!amount || !currency || !userId) {
             return res.status(400).json({ error: "Amount and currency are required" });
         }
 
         const paymentIntent = await stripe.paymentIntents.create({
+            userId,
             amount,
             currency,
             automatic_payment_methods: { enabled: true },
