bcb1088fe6530c24743e2dc860b0d1efa0672e21
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2025-03-30 22:47:07 +0100

    experiments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,27 +2,34 @@ import { Slot } from "expo-router";
 import { SafeAreaView, StyleSheet, ActivityIndicator } from "react-native";
 import { UserContextProvider } from "../contexts/userContext";
 import { useRootNavigationState, Redirect } from "expo-router";
+import { useSegments } from "expo-router";
 
 export default function RootLayout() {
   const navigationState = useRootNavigationState();
-  if (!navigationState?.key) {
-    // If the navigation state is not ready, we can return null or a loading indicator
-    return (
-      <ActivityIndicator
-        size="large"
-        color="#0000ff"
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      />
-    );
-  }
+  const segments = useSegments();
+
+  // if (!navigationState?.key) {
+  //   return (
+  //     <ActivityIndicator
+  //       size="large"
+  //       color="#0000ff"
+  //       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+  //     />
+  //   );
+  // }
+
+  // If we're at the root route (no segments), redirect to map
+  const isRootRoute = segments.length === 0;
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <Slot />
-    </SafeAreaView>
+    <UserContextProvider>
+      <SafeAreaView style={styles.safeArea}>
+        {/* {isRootRoute ? <Redirect href="/map" /> : <Slot />} */}
+        <Slot />
+      </SafeAreaView>
+    </UserContextProvider>
   );
 }
-
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
